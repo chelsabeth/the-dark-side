ad24ef3737edcce506bdef48df97eb091ceb5b0f
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-04 17:05:05 -0500

    setter function for useLocalStorage, moving onto useDarkMode

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,5 +5,11 @@ const useLocalStorage = (key, initialValue) => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
+    const setValue = value => {
+        // save state
+        setStoredValue(value);
+        // save to local storage
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
     return [storedValue, setValue]; 
 }
